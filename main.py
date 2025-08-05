53efcfbfcf28b7e5516c0d5b213d862fe9b57d03
baraaj <jeridibaraa@gmail.com>
2025-08-05 09:31:45 +0100

    dep commit

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     #app.run(debug=True)
     #app.run(debug=True, host='0.0.0.0', port=5000)
     if __name__ == "__main__":
-     app.run()
+      app.run(debug=True)
 
 #http://192.168.1.73/
